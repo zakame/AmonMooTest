ff7099e0c83b12c6c4d76eacf42e849775ecbd0f
Zak B. Elep <zak.elep@yoyo.ph>
2015-02-25 14:04:11 +0800

    Move out db() into a Moo::Role

--- lib/AmonMooTest.pm
+++ lib/AmonMooTest.pm
@@ -10,24 +10,7 @@ extends qw/Amon2/;
 # Enable project local mode.
 __PACKAGE__->make_local_context();
 
-my $schema = AmonMooTest::DB::Schema->instance;
-
-sub db {
-    my $c = shift;
-    if (!exists $c->{db}) {
-        my $conf = $c->config->{DBI}
-            or die "Missing configuration about DBI";
-        $c->{db} = AmonMooTest::DB->new(
-            schema       => $schema,
-            connect_info => [@$conf],
-            # I suggest to enable following lines if you are using mysql.
-            # on_connect_do => [
-            #     'SET SESSION sql_mode=STRICT_TRANS_TABLES;',
-            # ],
-        );
-    }
-    $c->{db};
-}
+with 'AmonMooTest::Database';
 
 1;
 __END__
---
+++ lib/AmonMooTest/Database.pm
@@ -0,0 +1,26 @@
+package AmonMooTest::Database;
+use Moo::Role;
+use utf8;
+
+requires 'config';
+
+my $schema = AmonMooTest::DB::Schema->instance;
+
+sub db {
+    my $c = shift;
+    if (!exists $c->{db}) {
+        my $conf = $c->config->{DBI}
+            or die "Missing configuration about DBI";
+        $c->{db} = AmonMooTest::DB->new(
+            schema       => $schema,
+            connect_info => [@$conf],
+            # I suggest to enable following lines if you are using mysql.
+            # on_connect_do => [
+            #     'SET SESSION sql_mode=STRICT_TRANS_TABLES;',
+            # ],
+        );
+    }
+    $c->{db};
+}
+
+1;
