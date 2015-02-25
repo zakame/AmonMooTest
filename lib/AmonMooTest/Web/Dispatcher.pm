9394eac186c9cc4aa656829cf7d197156f4436f9
Zak B. Elep <zak.elep@yoyo.ph>
2015-02-25 14:04:55 +0800

    Add a couple simple DB access paths

--- lib/AmonMooTest/Web/Dispatcher.pm
+++ lib/AmonMooTest/Web/Dispatcher.pm
@@ -25,4 +25,29 @@ post '/account/logout' => sub {
     return $c->redirect('/');
 };
 
+get '/hello/:name' => sub {
+    my ( $c, $args ) = @_;
+    if ( my $r
+        = $c->db->find_or_create( member => { name => $args->{name} } ) )
+    {
+        my $n = $r->name;
+        my $m = "Hello $n!";
+        if ( my $cnt = $c->session->get("counter_$n") || 0) {
+            $m = "Welcome back, $n!";
+        }
+        else {
+            $cnt++;
+            $c->session->set( "counter_$n", $cnt );
+        }
+        return $c->create_simple_status_page( 200, $m );
+    }
+};
+
+get '/bye/:name' => sub {
+    my ( $c, $args ) = @_;
+    my $n = $args->{name};
+    $c->session->remove("counter_$n");
+    $c->create_simple_status_page( 200, "Bye, $n!" );
+};
+
 1;
